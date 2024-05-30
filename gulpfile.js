8a55960f92ba43132c736f0da68671d75737303f
e-bryll <mail.4.eva.eva@google.com>
2024-05-30 06:23:15 +0300

    Update gulpfile to include HTML and assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const htmlmin = require('gulp-htmlmin');
 
-// Завдання для компіляції SASS в CSS
+// Task for compiling SASS to CSS
 gulp.task('styles', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/css'));
 });
 
-// Виклик gulp-задачі styles
-gulp.task('build', gulp.series('styles'));
+// Task for copying and minifying HTML files
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+// Task for copying other assets (images, JS, etc.)
+gulp.task('assets', function() {
+  return gulp.src('src/assets/**/*')
+    .pipe(gulp.dest('dist/assets'));
+});
+
+// Build task
+gulp.task('build', gulp.series('styles', 'html', 'assets'));
