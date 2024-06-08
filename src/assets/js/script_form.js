54fce3720ea89423f369e81837df79512c9cb693
e-bryll <mail.4.eva.eva@google.com>
2024-06-08 16:25:52 +0300

    Update

--- src/assets/js/script_form.js
+++ src/assets/js/script_form.js
@@ -9,51 +9,54 @@ const closeBtns = document.querySelectorAll('.close-btn');
 
 // Open form modal
 if (openFormModalBtn) {
-  openFormModalBtn.addEventListener('click', () => {
-    console.log('Open Form Modal Button Clicked');
-    formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
-  });
+    openFormModalBtn.addEventListener('click', () => {
+        console.log('Open Form Modal Button Clicked');
+        formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+        console.log('Form Modal State:', formModal.classList);
+    });
 }
 
 // Close modals
 closeBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    console.log('Close Button Clicked');
-    formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-    successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-  });
+    btn.addEventListener('click', () => {
+        console.log('Close Button Clicked');
+        formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+        successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+        console.log('Form Modal State:', formModal.classList);
+        console.log('Success Modal State:', successModal.classList);
+    });
 });
 
 // Form submission
 if (form) {
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log('Form Submitted');
-    const formData = new FormData(form);
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log('Form Submitted');
+        const formData = new FormData(form);
 
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => {
-        console.log('Form Submission Successful');
-        formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-        successModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
-        setTimeout(() => {
-          successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-        }, 2000);
-        clearFormFields();
-      })
-      .catch((error) => console.log('Sending form failed', error));
-  });
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams(formData).toString(),
+        })
+        .then(() => {
+            console.log('Form Submission Successful');
+            formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+            successModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+            setTimeout(() => {
+                successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+            }, 2000);
+            clearFormFields();
+        })
+        .catch((error) => console.log('Sending form failed', error));
+    });
 }
 
 // Clear form fields
 function clearFormFields() {
-  const modalFields = formModal.querySelectorAll('input, textarea');
+    const modalFields = formModal.querySelectorAll('input, textarea');
 
-  modalFields.forEach((field) => {
-    field.value = '';
-  });
+    modalFields.forEach((field) => {
+        field.value = '';
+    });
 }
